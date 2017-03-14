d06edf1ecdb796ab071527daa14afd60c7af0cc7
yangming <ming.yang@inno-Optics.com>
2017-03-14 15:30:39 +0800

    像素Dp互转 Signed-off-by: yangming <ming.yang@inno-Optics.com>

--- app/src/main/java/league/funny/com/funnyleague/util/Util.java
+++ app/src/main/java/league/funny/com/funnyleague/util/Util.java
@@ -1,5 +1,7 @@
 package league.funny.com.funnyleague.util;
 
+import android.content.Context;
+
 /**
  * Created by inno-y on 2017/2/24.
  */
@@ -13,6 +15,22 @@ public class Util {
                 .replace("<br/>",System.getProperty("line.separator"))
                 .replace("<br>",System.getProperty("line.separator"));
         return arg;
+    }
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dp2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
     }
 
 }
